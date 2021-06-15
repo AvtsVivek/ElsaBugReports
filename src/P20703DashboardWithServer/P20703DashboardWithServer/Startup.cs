b59d85c829803f8d1216cdbf27ac83606574f1c5
Vivek Koppula <aaryavarttechsolutions@gmail.com>
2021-06-15 16:33:27 +0530

    Removed the comments from start up file

--- src/P20703DashboardWithServer/P20703DashboardWithServer/Startup.cs
+++ src/P20703DashboardWithServer/P20703DashboardWithServer/Startup.cs
@@ -19,24 +19,14 @@ namespace P20703DashboardWithServer
             Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             var elsaSection = Configuration.GetSection("Elsa");
 
-            // Elsa services.
             services
                 .AddElsa(elsa => elsa
-                    //.UseEntityFrameworkPersistence(ef => ef.UseSqlite())
                     .UseEntityFrameworkPersistence(ef =>
                     {
-                        //ef.UseSqlite();
-
-                        //ef.UsePostgreSql("Server=127.0.0.1;Port=5432;Database=elsa;User Id=postgres;Password=password;");
-
-                        //ef.UseMySql("Server=localhost;Port=3306;Database=elsa;User=root;Password=password;");
-
                         ef.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Elsa20;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
                     })
                     .AddConsoleActivities()
@@ -45,45 +35,25 @@ namespace P20703DashboardWithServer
                     .AddWorkflowsFrom<Startup>()
                 );
 
-            // Elsa API endpoints.
             services.AddElsaApiEndpoints();
-
-            // For Dashboard.
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (Environment.IsDevelopment())
                 app.UseDeveloperExceptionPage();
-            
-
-            //app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
-
-
-
             app
                 .UseStaticFiles() // For Dashboard.
                 .UseHttpActivities()
                 .UseRouting()
                 .UseEndpoints(endpoints =>
                 {
-                    // Elsa API Endpoints are implemented as regular ASP.NET Core API controllers.
                     endpoints.MapControllers();
                     endpoints.MapControllerRoute(
                         name: "default",
                         pattern: "{controller=Home}/{action=Index}/{id?}");
 
-                    // For Dashboard.
                     endpoints.MapFallbackToPage("/_Host");
                     //endpoints.MapFallbackToPage("/ElsaWorkflows/{**segment}", "/_Host");
                 });
